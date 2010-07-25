519a98eb902489d66446339937164054792f0889
Fabrizio Corvace <fabrizio@computersostituto.(none)>
2010-07-25 16:38:18 +0200

    Update the whole game, with a decent background, ball and base, increased speed and lifted up the base from the ground

--- gamepy.py
+++ gamepy.py
@@ -2,16 +2,17 @@ import sys, pygame ,time
 pygame.init()
 life=3
 size = width, height = 1024,600
-speed = [2,2]
+speed = [10,10]
 speedb = [0,0]
-black = 0, 0, 0
 screen = pygame.display.set_caption='OCoA'
 screen = pygame.display.set_mode(size)
+back = pygame.image.load("background.png")
 ball = pygame.image.load("ball.png")
 base = pygame.image.load("base.png")
 baserect = base.get_rect()
 ballrect = ball.get_rect()
-baserect.bottom=height
+backrect= back.get_rect()
+baserect.bottom=height-10
 baserect.right=(width)/2+40
 while life>0:
 	for event in pygame.event.get():
@@ -21,9 +22,9 @@ while life>0:
                         pygame.quit()
                         sys.exit()                
                 if key[pygame.K_a]:
-                        speedb=[-4,0]
+                        speedb=[-10,0]
                 if key[pygame.K_d]:
-                        speedb=[+4,0]                        
+                        speedb=[+10,0]                        
    	ballrect = ballrect.move(speed)
    	baserect = baserect.move(speedb)
     	if ballrect.left < 0 or ballrect.right > width:
@@ -34,7 +35,7 @@ while life>0:
         	speed[1] = -speed[1]
         if baserect.left < 0 or baserect.right > width:
                 speedb=[0,0]
-        screen.fill(black)
+        screen.blit(back,backrect)
 	screen.blit(ball, ballrect)
 	screen.blit(base, baserect)
 	pygame.display.flip()
