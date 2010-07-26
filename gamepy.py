37ee1c901e2fea3217cc42e50f729ed72d5d11bb
Marco Donato Torsello <md1512@gmail.com>
2010-07-26 13:56:54 +0200

    Mouse pointer

--- gamepy.py
+++ gamepy.py
@@ -4,9 +4,9 @@ life=3
 rebound=0
 point=0
 size = width, height = 1024,600
-speed = [5,5]
+speed = [10,10]
 speedb = [0,0]
-screen = pygame.display.set_caption='OCoA'
+screen = pygame.display.set_caption('OCoA')
 screen = pygame.display.set_mode(size)
 back = pygame.image.load("background.png")
 ball = pygame.image.load("ball.png")
@@ -22,15 +22,18 @@ while life>0:
         	if event.type == pygame.QUIT or key[pygame.K_ESCAPE]: #Se premi la croce chiude 
                         print "Vite="+str(life)+" Rimbalzi="+str(rebound)
                         pygame.quit()
-                        sys.exit()                
-                if key[pygame.K_a]:
-                        baserect.left=baserect.left-width/8      
-                if key[pygame.K_d]:
-                        baserect.right=baserect.right+width/8
-                if key[pygame.K_LEFT]:
-                        baserect.left=baserect.left-width/4
-                if key[pygame.K_RIGHT]:
-                        baserect.right=baserect.right+width/4
+                        sys.exit()
+                loc=pygame.mouse.get_pos()
+                #print loc[0]
+                #if key[pygame.K_a]:
+                #        baserect.left=baserect.left-width/8      
+                #if key[pygame.K_d]:
+                #        baserect.right=baserect.right+width/8
+                #if key[pygame.K_LEFT]:
+                #        baserect.left=baserect.left-width/4
+                #if key[pygame.K_RIGHT]:
+                #        baserect.right=baserect.right+width/4
+                baserect.left=loc[0]-40
    	ballrect = ballrect.move(speed)
     	if ballrect.left < 0 or ballrect.right > width:
         	speed[0] = -speed[0]
@@ -46,7 +49,7 @@ while life>0:
         if ((ballrect.right>=baserect.left and ballrect.right<=baserect.right)or(ballrect.left>=baserect.left and ballrect.left<=baserect.right)) and  baserect.top<=ballrect.bottom:
                 speed[1] = -speed[1]
                 rebound+=1
-                point+=point+rebound**life
+                #point+=point+rebound**life
         screen.blit(back,backrect)
 	screen.blit(ball, ballrect)
 	screen.blit(base, baserect)
