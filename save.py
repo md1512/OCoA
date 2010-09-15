9a0317f1abb6a6911b605352bf035bf062bf1a38
Marco Donato Torsello <md1512@gmail.com>
2010-09-15 19:21:42 +0200

    Debugged save

--- save.py
+++ save.py
@@ -1,8 +1,7 @@
 import time,sqlite3,os
 PATH=os.environ['HOME']+"/.ocoa/"
 def save(name,score):
-    res=0
-    #try:
+    res=0    
     conn = sqlite3.connect(PATH+"saved")#Database
     print PATH+"saved"
     c = conn.cursor()
@@ -11,15 +10,22 @@ def save(name,score):
         print "SELECT COUNT(id) FROM USER WHERE name=\'"+str(name)+"\';"
     except:
         print "maccione save"
-    SQLnum=int(c.fetchall()[0][0])
+    SQLnum=int(c.fetchone()[0])
     print SQLnum
     if SQLnum>0 :
-        SQLid=long(c.fetchone()[0])		
+        SQLid=SQLnum+1
         print SQLid
     else:
         c.execute("SELECT MAX(id) FROM USER;")
         print "SELECT MAX(id) FROM USER;"
-        SQLid=long(c.fetchone()[0])+1
+        a=c.fetchone()[0]
+        #if a[0]!="None":
+        #print a
+        try:
+            SQLid=long(a)+1
+        except:
+            SQLid=1
+#            print a[0]
         print SQLid
     c.execute("INSERT INTO USER VALUES(\'"+name+"\',"+str(SQLid)+");")
     print "INSERT INTO USER VALUES(\'"+name+"\',"+str(SQLid)+");"
@@ -27,6 +33,6 @@ def save(name,score):
     print "INSERT INTO SCORE VALUES("+str(score)+","+str(SQLid)+","+str(time.ctime())+");"
     c.close()
     conn.commit()
-    conn.close()
-    #except:
-    #    print "massimiliano save"
+    conn.close()    
+def hiscore():
+    print "mac"
