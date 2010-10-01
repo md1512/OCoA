be519254a389a554746cf63a9a003f445acb9b79
Marco Donato Torsello <md1512@gmail.com>
2010-10-01 15:40:09 +0200

    Corrected a bug

--- save.py
+++ save.py
@@ -2,33 +2,29 @@ import time,sqlite3,os
 import configuration
 
 def save(name,score):
-    res=0    
+    res=0
     conn = sqlite3.connect(configuration.PATHDB)#Database    
     c = conn.cursor()
-    try :
-        c.execute("SELECT COUNT(id) FROM USER WHERE name=\'"+str(name)+"\';")   
-        print "SELECT COUNT(id) FROM USER WHERE name=\'"+str(name)+"\';"
-    except:
-        print "maccione save"
-    SQLnum=int(c.fetchone()[0])    
-    if SQLnum>0 :
-        SQLid=SQLnum+1
-        print SQLid
-    else:
+    c.execute("SELECT id FROM USER WHERE name=\'"+str(name)+"\';")
+    a=c.fetchone()
+    if a==None:
         c.execute("SELECT MAX(id) FROM USER;")
         print "SELECT MAX(id) FROM USER;"
-        a=c.fetchone()[0]        
-        try:
-            SQLid=long(a)+1
-        except:
+        b=c.fetchone()[0]
+        print b
+        if b==None:
             SQLid=1
-    c.execute("INSERT INTO USER VALUES(\'"+name+"\',"+str(SQLid)+");")
-    print "INSERT INTO USER VALUES(\'"+name+"\',"+str(SQLid)+");"
+        else:
+            SQLid=long(b)+1
+        c.execute("INSERT INTO USER VALUES(\'"+name+"\',"+str(SQLid)+");")
+        print "INSERT INTO USER VALUES(\'"+name+"\',"+str(SQLid)+");"
+    else:
+        SQLid=long(a[0])
     c.execute("INSERT INTO SCORE VALUES("+str(score)+","+str(SQLid)+",\'"+str(time.ctime())+"\');")	
     print "INSERT INTO SCORE VALUES("+str(score)+","+str(SQLid)+","+str(time.ctime())+");"
     c.close()
     conn.commit()
-    conn.close()    
+    conn.close()   
 def hiscore():
     t=[]
     conn = sqlite3.connect(configuration.PATHDB)#Database    
@@ -39,7 +35,7 @@ def hiscore():
         print len(a),a
         for i in a:
             print "SELECT name FROM USER WHERE id="+str(i[1])
-            c.execute("SELECT name FROM USER WHERE id="+str(i[1]))
+            c.execute("SELECT name FROM USER WHERE id="+str(i[1])+";")
             b=c.fetchone()
             print i[0],b[0]
             t.append(str(b[0])+ ":"+str(i[0]))
